80399ba800f8d1ce6fc284e9386f3d79ba4b845b
babay_fv72 <babay_fv72@STHQ0128-16>
2025-09-07 14:40:55 +0400

    Add project files.

---
+++ DbPractice/Services/Abstract/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using DbPractice.Model;
+
+namespace DbPractice.Services.Abstract;
+
+public interface IProductService
+{
+    public void Add(Product item);
+    public void Remove(Product item);
+    public Product GetById(int id);
+    public List<Product> GetAll();
+}
---
+++ DbPractice/Services/CustomerService.cs
@@ -0,0 +1,36 @@
+﻿using DbPractice.DataBase;
+using DbPractice.Model;
+using DbPractice.Services.Abstract;
+
+namespace DbPractice.Services;
+
+public class CustomerService : BaseService, ICustomerService
+{
+    public CustomerService(StoreAppDataBase context) : base(context)
+    {
+    }
+
+    public void Add(Customer item)
+    {
+        _context.Customers.Add(item);
+    }
+
+    public List<Customer> GetAll()
+    {
+        return _context.Customers.ToList();
+    }
+
+    public Customer GetById(int id)
+    {
+        return _context.Customers.Find(id);
+    }
+    public void Remove(Customer item)
+    {
+       var customer = _context.Customers.Find(item.Id);
+        if (customer != null)
+        {
+            customer.IsDeleted = true;
+            customer.DeletedDate = DateTime.Now;
+        }
+    }
+}
